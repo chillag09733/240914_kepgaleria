6f1e94cbdecd727236658d528331e260e7fe523e
chillag09733 <chillag09733@gmail.com>
2024-09-14 11:19:53 +0200

    html létrehozása

---
+++ kepek.js
@@ -0,0 +1,47 @@
+export const kepek = [
+    {
+        nev: "Waning Gibbous",
+        felirat: "Waning Gibbous",
+        link: "1.jpg"
+    },
+    {
+        nev: "Total eclipse",
+        felirat: "Total eclipse",
+        link: "2.jpg"
+    },
+    {
+        nev: "Waxing Crescent",
+        felirat: "Waxing Crescent",
+        link: "3.jpg"
+    },
+    {
+        nev: "Moon phase",
+        felirat: "Moon phase",
+        link: "4.jpg"
+    },
+    {
+        nev: "First Quarter",
+        felirat: "First Quarter",
+        link: "5.jpg"
+    },
+    {
+        nev: "Clouds",
+        felirat: "Clouds",
+        link: "6.jpg"
+    },
+    {
+        nev: "Waxing Crescent 2",
+        felirat: "Waxing Crescent 2",
+        link: "7.jpg"
+    },
+    {
+        nev: "Moon Phases",
+        felirat: "Moon Phases",
+        link: "8.jpg"
+    },
+    {
+        nev: "Full Moon",
+        felirat: "Full Moon",
+        link: "9.jpg"
+    },
+]
